922a6a1289103bd80e6e3325938b8c0e6bd25927
dukesp69 <dukesp69@gmail.com>
2023-04-10 17:25:48 +0200

    Show calendar

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -49,9 +49,10 @@ func routes(app *config.AppConfig) http.Handler {
 		mux.Get("/reservations-new", handlers.Repo.AdminNewReservation)
 		mux.Get("/reservations-all", handlers.Repo.AdminAllReservation)
 		mux.Get("/reservations-calendar", handlers.Repo.AdminReservationsCalendar)
-		mux.Get("/process-reservation/{src}/{id}", handlers.Repo.AdminProcessedReservation)
-		mux.Get("/delete-reservation/{src}/{id}", handlers.Repo.AdminDeleteReservation)
-		mux.Get("/reservations/{src}/{id}", handlers.Repo.AdminShowReservation)
+		mux.Post("/reservations-calendar", handlers.Repo.AdminPostReservationsCalendar)
+		mux.Get("/process-reservation/{src}/{id}/do", handlers.Repo.AdminProcessedReservation)
+		mux.Get("/delete-reservation/{src}/{id}/do", handlers.Repo.AdminDeleteReservation)
+		mux.Get("/reservations/{src}/{id}/show", handlers.Repo.AdminShowReservation)
 		mux.Post("/reservations/{src}/{id}", handlers.Repo.AdminPostShowReservation)
 
 	})
